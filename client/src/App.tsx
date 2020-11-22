a5cd9692dae91e0702010e07c804d5329e38d0e5
Pato <patotomastoledo6@gmail.com>
2020-11-22 15:04:13 -0300

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,22 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import  Main  from "./views/Main"
 import LogIn from "./views/LogIn"
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <LogIn />
-      <Main />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/login" component={LogIn} />
+            <Route exact path="/home" component={Main} />
+          </Switch>
+        </div>
+      </Router>
+
+
     </div>
   );
 }
