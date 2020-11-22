4da22b678fe3c39ab0f8b756588f8202bbc49722
redbaron23 <pato>
2020-11-22 17:59:13 -0300

    redux

--- client/src/views/LogIn.tsx
+++ client/src/views/LogIn.tsx
@@ -1,20 +1,49 @@
 import React, { useState } from "react";
-import Amplify, { Storage } from "aws-amplify";
+import Amplify, { Hub,Logger } from "aws-amplify";
 import config from "../aws-exports";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import { UploadPicture } from "../hooks/UploadPicture"
+import { UploadPicture } from "../hooks/UploadPicture";
 import { withSideBar } from "../hooks/withSideBar";
 Amplify.configure(config);
 
+const logger = new Logger("My-Logger");
+
+const listener = (data:any) => {
+  console.log(data);
+  switch (data.payload.event) {
+    case "signIn":
+      alert('entro');
+      logger.info("user signed in entro");
+      break;
+    case "signUp":
+      logger.info("user signed up");
+      break;
+    case "signOut":
+      logger.info("user signed out");
+      break;
+    case "signIn_failure":
+      logger.error("user sign in failed");
+      break;
+    case "tokenRefresh":
+      logger.info("token refresh succeeded");
+      break;
+    case "tokenRefresh_failure":
+      logger.error("token refresh failed");
+      break;
+    case "configured":
+      logger.info("the Auth module is configured");
+  }
+};
+
+Hub.listen("auth", listener);
+
 interface Props {}
 
-
 const LogIn: React.FC<Props> = () => {
-
   return (
     <div>
       <UploadPicture />
-   </div>
+    </div>
   );
 };
 
