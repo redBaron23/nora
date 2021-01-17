8631bbd181c2e0fe0dd13e31c51fcfb2114eeee5
redbaron23 <pato>
2021-01-17 00:25:29 -0300

    sign up

--- client/src/views/LogIn.tsx
+++ client/src/views/LogIn.tsx
@@ -1,14 +1,39 @@
 import React, { useState } from "react";
-import Amplify, { Hub,Logger } from "aws-amplify";
+import Amplify, { Hub, Logger } from "aws-amplify";
 import config from "../aws-exports";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { AmplifyAuthenticator, AmplifySignOut,AmplifySignIn } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 import { UploadPicture } from "../hooks/UploadPicture";
 import { withSideBar } from "../hooks/withSideBar";
 Amplify.configure(config);
 
+const LogIn = () => {
+  const [authState, setAuthState] = useState<AuthState | null>(null);
+  const [user, setUser] = useState<object | null | undefined>(null);
+
+  React.useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <div>
+      <UploadPicture />
+      <AmplifySignOut />
+    </div>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignIn slot="sign-in" hideSignUp={true}></AmplifySignIn>
+    </AmplifyAuthenticator>
+  );
+};
+
 const logger = new Logger("My-Logger");
 
-const listener = (data:any) => {
+const listener = (data: any) => {
   console.log(data);
   switch (data.payload.event) {
     case "signIn":
@@ -35,15 +60,5 @@ const listener = (data:any) => {
 };
 
 Hub.listen("auth", listener);
-
-interface Props {}
-
-const LogIn: React.FC<Props> = () => {
-  return (
-    <div>
-      <UploadPicture />
-    </div>
-  );
-};
 
 export default withSideBar(withAuthenticator(LogIn));
