0dd524750f1a958d45806a647b0a3eed4fbf9041
redbaron23 <pato>
2021-01-30 17:05:02 -0300

    Modal

--- client/src/hooks/PictureModal.tsx
+++ client/src/hooks/PictureModal.tsx
@@ -1,54 +1,51 @@
 import React from "react";
-import { Button, Image, Modal } from "semantic-ui-react";
+import { Button, Image, Modal, Divider } from "semantic-ui-react";
 import { Picture } from "./Picture";
 import { IPicture } from "../interfaces";
-import { CustomIcon } from "./CustomIcon"
 
 interface Props {
   picture: IPicture;
 }
 
+const defaultDescription = 'Para consultar si este cuadro esta a la venta contacteme a mis redes sociales haciendo click en los iconos. O bien puede enviarme un email a norilla07@yahoo.com.ar'
+
 export const PictureModal: React.FC<Props> = ({ picture }) => {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <div>
-      <Modal
-        onClose={() => setOpen(false)}
-        basic
-        className="block-example border border-primary"
-        size="fullscreen"
-        onOpen={() => setOpen(true)}
-        open={open}
-        trigger={
-          <div onClick={() => setOpen(true)}>
-            <Picture picture={picture} />
-          </div>
-        }
-      >
-        <Modal.Header>{picture.title}</Modal.Header>
-        <Modal.Content>
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <Image size="large" src={picture.src} wrapped />
-              </div>
-              <div className="col-6">
-                <p>{picture.description}</p>
-              </div>
-              <div className="col-12">
-                <CustomIcon />
-              </div>
-            </div>
-          </div>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
-          <Button onClick={() => setOpen(false)} positive>
-            Ok
-          </Button>
-        </Modal.Actions>
-      </Modal>
-    </div>
+    <Modal
+      onClose={() => setOpen(false)}
+      basic
+      className="block-example border border-primary"
+      size="fullscreen"
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={
+        <div onClick={() => setOpen(true)}>
+          <Picture picture={picture} />
+        </div>
+      }
+    >
+      <Modal.Header>
+        <p className="text-primary align-self-right">{picture.title}</p>
+      </Modal.Header>
+      <Modal.Content image>
+        <div className="container">
+          <Image size="huge" src={picture.src} wrapped />
+          <Divider inverted />
+          <Modal.Description>
+            <p>{(!!picture.description ? picture.description : defaultDescription)}</p>
+          </Modal.Description>
+        </div>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          onClick={() => setOpen(false)}
+          className="bg-primary text-light"
+        >
+          Cerrar
+        </Button>
+      </Modal.Actions>
+    </Modal>
   );
 };
